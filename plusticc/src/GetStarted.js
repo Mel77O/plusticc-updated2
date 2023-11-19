bdb835dec09dc160d8bec5e40dc12c824e090f07
dedensyaaa <151383042+dedensyaaa@users.noreply.github.com>
2023-11-20 01:44:21 +0800

    Update GetStarted.js by Morales

--- plusticc/src/GetStarted.js
+++ plusticc/src/GetStarted.js
@@ -1,12 +1,12 @@
 // GetStarted.js
 import React, { useState } from 'react';
 import './GetStarted.css';
-import Login from './Login';
 import backgroundImage from './background.jpg';
 import { useNavigate } from 'react-router-dom';
+import Header from './Header';
 
 const GetStarted = () => {
-  const [showLogin, setShowLogin] = useState(false);
+  
 
   const navigate = useNavigate();
 
@@ -15,27 +15,33 @@ const GetStarted = () => {
     navigate('/login');
   };
 
+  const handleRegisterClick = () => {
+    // Navigate to the '/login' route
+    navigate('/register');
+  };
+
   return (
-    <div className="get-started-container">
-      {/* Left grid with background image */}
-      <div className="left-grid" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
+    <div>
+      <Header />
+      <div className="get-started-container">
+        {/* Left grid with background image */}
+        <div className="left-grid" style={{ backgroundImage: `url(${backgroundImage})` }}></div>
 
-      {/* Right grid with text and buttons */}
-      <div className="right-grid">
-        <h1>Get started with +TIC</h1>
-        <p>"The only way forward is to treat plastic as a valuable resource, not as disposable waste. Let's manage it wisely for a cleaner, greener tomorrow."</p>
+        {/* Right grid with text and buttons */}
+        <div className="right-grid">
+          <h1>Get started with +TIC</h1>
+          <h3>"The only way forward is to treat plastic as a valuable resource, not as disposable waste. Let's manage it wisely for a cleaner, greener tomorrow."</h3>
 
-        <div className="buttons-container">
-          <button className="sign-up-button">Register</button> -or-
-          <button className="login-button" onClick={handleLoginClick}>Login</button>
+          <div className="buttons-container">
+            <button className="sign-up-button" onClick={handleRegisterClick}>Register</button> <p>-or-</p>
+            <button className="login-button" onClick={handleLoginClick}>Login</button>
+          </div>
+
+          
         </div>
-
-        {showLogin && <Login />}
       </div>
     </div>
   );
 };
 
 export default GetStarted;
-
-// Rest of the code remains the same...

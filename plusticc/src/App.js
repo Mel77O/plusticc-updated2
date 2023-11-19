44f86d43daff77948e05b6ac5f414b40494ec34a
dedensyaaa <151383042+dedensyaaa@users.noreply.github.com>
2023-11-20 01:39:55 +0800

    Update App.js

--- plusticc/src/App.js
+++ plusticc/src/App.js
@@ -3,43 +3,19 @@ import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-route
 import Header from './Header';
 import GetStarted from './GetStarted';
 import Login from './Login';
+import Register from './Register';
 
 function App() {
-    const [showHeader, setShowHeader] = useState(true);
-
-    // Add a listener to handle route changes
-    const handleRouteChange = () => {
-        // Check the current route and decide whether to show the Header
-        const currentPath = window.location.pathname;
-        if (currentPath === '/login') {
-        // Hide Header on the login page
-        setShowHeader(false);
-        } else {
-        // Show Header on other pages
-        setShowHeader(true);
-        }
-    };
-
-    // Use useEffect to add and remove the route change listener
-    React.useEffect(() => {
-        // Add the listener
-        window.addEventListener('popstate', handleRouteChange);
-
-        // Remove the listener when the component is unmounted
-        return () => {
-        window.removeEventListener('popstate', handleRouteChange);
-        };
-    }, []); // Empty dependency array means the effect runs once after the initial render
+    
 
     return (
         <Router>
         <div>
             {/* Conditionally render the Header based on the route */}
-            {showHeader && <Header />}
-
             <Routes>
-            <Route path="/login" element={<Login />} />
             <Route path="/" element={<GetStarted />} />
+            <Route path="/login" element={<Login />} /> 
+            <Route path="/register" element={<Register />} /> 
             </Routes>
         </div>
         </Router>

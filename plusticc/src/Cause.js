44f7efe11bc19844d20b806cf5d26e423298311d
kolay404 <angelnicole.b11@gmail.com>
2023-12-19 22:45:16 +0800

    added cause

--- plusticc/src/Cause.js
+++ plusticc/src/Cause.js
@@ -10,8 +10,42 @@ const Cause = () => {
       </div>
 
       <div className="cause-lower-container">
-        {/* Add your content for Type component here */}
-        <h2>HATDOG</h2>
+        <ul>
+      <h2>Cause and Effect of Plastic Waste</h2>
+      <h2>Cause of Plastic Waste</h2>
+      <p>1. Single-Use Culture:
+        <li>The convenience of single-use plastics, such as bottles, bags, and packaging, has led to a culture of disposability, contributing to the proliferation of plastic waste.</li></p> 
+        <p>2. Inadequate Recycling Infrastructure:
+        <li>Limited recycling facilities and inconsistent recycling practices can result in a significant portion of plastic waste ending up in landfills or the environment.</li></p>
+        <p>3. Lack of Awareness:
+        <li>Many people may not be fully aware of the environmental impact of plastic waste, leading to improper disposal and a lack of motivation to reduce plastic consumption.</li></p>
+        <p>4. Excessive Packaging:
+        <li>Overuse of packaging in products, both for protection and marketing purposes, generates a considerable amount of plastic waste.</li></p>
+        <p>5. Poor Waste Management Practices:
+        <li>Inadequate waste management systems, particularly in some regions and developing countries, can contribute to plastic litter and pollution.</li></p>
+        <p>6. Fast Fashion and Microfiber Pollution:
+        <li>The fashion industry's use of synthetic fibers and microplastics in clothing contributes to plastic pollution when these items shed tiny particles during washing.</li></p>
+        <p>7. Global Production of Plastics:
+        <li>The sheer volume of plastic production, driven by industrial and consumer demand, contributes significantly to the accumulation of plastic waste.</li></p>
+        <p>8. Improper Disposal:
+        <li>Improper disposal of plastic items, such as littering, increases the likelihood of plastic entering waterways and oceans.</li></p>
+        <p>9. Environmental Pollution:
+        <li>Plastic waste contaminates ecosystems, harming wildlife and disrupting natural habitats. Marine life, in particular, is affected by plastic pollution in oceans and waterways.</li></p>
+        <p>10. Health Risks:
+        <li>The breakdown of plastics can release harmful chemicals, some of which are known to be endocrine disruptors, posing potential risks to human health.</li></p>
+        <p>11. Microplastics in Food Chain:
+        <li>Microplastics, resulting from the breakdown of larger plastic items, can enter the food chain, impacting both aquatic and terrestrial organisms and potentially reaching humans.</li></p>
+        <p>12. Habitat Destruction:
+        <li>Plastic pollution can lead to the destruction of habitats, especially in marine environments, affecting the balance of ecosystems.</li></p>
+        <p>13. Economic Impact:
+        <li>The cleanup and management of plastic waste have economic repercussions, impacting industries such as tourism, fisheries, and waste management.</li></p>
+        <p>14. Long Decomposition Period:
+        <li>Many plastics take hundreds of years to decompose, contributing to the long-lasting environmental impact of plastic waste.</li></p>
+        <p>15. Air Pollution from Incineration:
+        <li>Incinerating plastics can release toxic fumes into the air, contributing to air pollution and potential health hazards for nearby communities.</li></p>
+        <p>16. Aesthetic and Recreational Impact:
+        <li>Plastic litter can negatively affect the aesthetic appeal of natural spaces and impact recreational activities, reducing the quality of life for communities.</li></p>
+        </ul>
       </div>
     </div>
   );

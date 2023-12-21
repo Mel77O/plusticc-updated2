3bae0c03d9414a81db3eca398947eee531e91640
dedensyaaa <151383042+dedensyaaa@users.noreply.github.com>
2023-12-21 11:35:11 +0800

    Update Login.js

--- plusticc/src/Login.js
+++ plusticc/src/Login.js
@@ -1,37 +1,86 @@
-// Login.js
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.css'; // Import your CSS file for styling
+import Header from './Header'
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+  
+      if (!response.ok) {
+        const errorData = await response.json();
+        setError(errorData.message);
+        return;
+      }
+  
+      const userDetails = await response.json();
+      console.log('Fetched UserDetails:', userDetails);
+  
+      setLoggedInUser(userDetails);
+      setLoading(true);
+    } catch (error) {
+      console.error('Login error:', error.message);
+      setError('Invalid email or password. Please try again.');
+    }
+  };
+  
+  useEffect(() => {
+    if (loading && loggedInUser) {
+      console.log('LoggedInUser', loggedInUser);
+      // Perform any other actions after setting loggedInUser
+      // Navigate to '/home'
+      navigate('/home');
+    }
+  }, [loggedInUser, loading]);
+//ey
   return (
-    <div className="login-container">
-      <div className="background-image" />
+    <div>
+      
+      <div className="login-container">
+        <div className="background-image" />
 
-      <div className="login-box">
-        <h1>Welcome to +TIC</h1>
-        <p>Login and help clean our environment!</p>
+        <div className="login-box">
+          <h1>Welcome to +TIC</h1>
+          <p>Login and help clean our environment!</p>
 
-        {/* Login form */}
-        <form>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
+          {/* Display error message */}
+          {error && <div className="error-message">{error}</div>}
 
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
+          {/* Login form */}
+          <form onSubmit={(e) => { e.preventDefault(); handleLogin(); }}>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" onChange={(e) => setEmail(e.target.value)} required />
 
-          <button type="submit">Login</button>
-        </form>
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)} required />
 
-        <div className="extra-options">
-          <label className="remember-me">
-            <input type="checkbox" />
-            <span>Remember me</span>
-          </label>
+            <button type="submit">Login</button>
+          </form>
+
+          <div className="extra-options">
+            <label className="remember-me">
+              <input type="checkbox" />
+              <span>Remember me</span>
+            </label>
+          </div>
+
           <span className="forgot-password">Forgot password?</span>
+          <h4>Don't have an account yet?<Link to="/register">Register here</Link></h4>
         </div>
-
-        <h4>Don't have an account yet?<Link to="/register">Register here</Link></h4>
       </div>
     </div>
   );
